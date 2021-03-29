d96305c6920239cdb8c0f1503d168ccf888773c3
Mikurio <Mikurio077@gmail.com>
2021-03-29 12:55:48 +0800

    pengalihmode

---
+++ app/src/main/java/com/example/latihantombolsederhana/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.latihantombolsederhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Tombol sudah ditekan", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
